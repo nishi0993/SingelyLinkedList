65e72e54a15b6749c17ffb3db8d09327f68f98e8
nishi0993 <nishi0993@gmail.com>
2019-02-26 00:51:19 -0800

    add constructor for data

--- src/main/SingelyLinkedList.java
+++ src/main/SingelyLinkedList.java
@@ -21,5 +21,9 @@ public class SingelyLinkedList {
         public void setNext(Node next) {
             this.next = next;
         }
+
+        public Node(int data) {
+            this.data = data;
+        }
     }
 }
