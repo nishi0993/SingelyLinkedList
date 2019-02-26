d76b26ddc14c01de41e8c37da0620271a6625bbd
nishi0993 <nishi0993@gmail.com>
2019-02-26 04:12:41 -0800

    add method deletHead

--- src/main/SingelyLinkedList.java
+++ src/main/SingelyLinkedList.java
@@ -1,8 +1,7 @@
 package main;
 
 public class SingelyLinkedList {
-    private class Node
-    {
+    private class Node {
         private int data;
         private Node next;
 
@@ -32,64 +31,73 @@ public class SingelyLinkedList {
         }
 
     }
-    private Node head=null;
-    private int size=0;
+
+    private Node head = null;
+    private int size = 0;
 
     public static void main(String[] args) {
-        SingelyLinkedList linkedList=new SingelyLinkedList();
+        SingelyLinkedList linkedList = new SingelyLinkedList();
         System.out.println(linkedList);
-        linkedList.insert(66);
-       linkedList.insert(42);
+//        linkedList.insert(66);
+//        linkedList.deleteHead();
+//        linkedList.insert(42);
        /* for (int i = 0; i < 5; i++) {
             linkedList.insert(i+1);
         }*/
+        for (int i = 0; i < 5; i++) {
+            linkedList.insert(i + 1);
+        }
+        System.out.println(linkedList);
+        linkedList.deleteHead();
         System.out.println(linkedList);
     }
 
     @Override
     public String toString() {
-        StringBuilder response= new StringBuilder();//toreduce immutability of String
+        StringBuilder response = new StringBuilder();//toreduce immutability of String
         response.append("[");//to have brackets while printing
-        Node temp=this.head;//copy so that actual head do not change
-        while(temp !=null)
-        {
+        Node temp = this.head;//copy so that actual head do not change
+        while (temp != null) {
             response.append(temp.getData());//add data in response
-            if(temp.next!=null)
-            {
+            if (temp.next != null) {
                 response.append(" ==> ");
             }
-            temp=temp.next;
+            temp = temp.next;
         }
         response.append("]");
         return response.toString();
     }
-    private void insertHead(int data)
-    {
-      Node newNode=new Node(data,this.head);
-      this.head=newNode;
-      size++;
-    }
-    private void insertAfter(int data,Node node)
-    {
-        Node newNode=new Node(data,node.next);
-        node.next=newNode;//or node.next=new Node(data,node.next)
+
+    private void insertHead(int data) {
+        Node newNode = new Node(data, this.head);
+        this.head = newNode;
         size++;
     }
-    public void insert(int data)
-    {
-        if(head==null)
-        {
+
+    private void insertAfter(int data, Node node) {
+        Node newNode = new Node(data, node.next);
+        node.next = newNode;//or node.next=new Node(data,node.next)
+        size++;
+    }
+
+    public void insert(int data) {
+        if (head == null) {
             insertHead(data);
-        }
-        else
-        {
-            Node temp=this.head;
-            while(temp.next!=null)
-            {
-                temp=temp.next;
+        } else {
+            Node temp = this.head;
+            while (temp.next != null) {
+                temp = temp.next;
             }
             insertAfter(data, temp);
         }
 
     }
+
+    void deleteHead() {
+        Node temp = head.next;
+        head.next = null;
+        head = temp;
+        size--;
+    }
+
 }
