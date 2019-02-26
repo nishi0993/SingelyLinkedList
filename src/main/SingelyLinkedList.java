091f7a1ab34cbe1cdbae0502eb55ed0ad1cc53a0
nishi0993 <nishi0993@gmail.com>
2019-02-26 00:50:22 -0800

    add getter, setter for data and next

--- src/main/SingelyLinkedList.java
+++ src/main/SingelyLinkedList.java
@@ -6,5 +6,20 @@ public class SingelyLinkedList {
         private int data;
         private Node next;
 
+        public int getData() {
+            return data;
+        }
+
+        public void setData(int data) {
+            this.data = data;
+        }
+
+        public Node getNext() {
+            return next;
+        }
+
+        public void setNext(Node next) {
+            this.next = next;
+        }
     }
 }
